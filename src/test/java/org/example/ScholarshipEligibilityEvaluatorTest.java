5681040a5e8d1b61ad6f4c739a819cc627bc9047
João Pedro da Silva Nascimento <joaopedronascimento171@gmail.com>
2026-03-23 09:56:38 -0300

    First test

--- src/test/java/org/example/ScholarshipEligibilityEvaluatorTest.java
+++ src/test/java/org/example/ScholarshipEligibilityEvaluatorTest.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScholarshipEligibilityEvaluatorTest {
+    Random random = new Random();
+    @Test
+    void putAgeLowerThanMinimum(){
+        var age = random.nextInt(15);
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                age,
+                8.5,
+                92.0,
+                true,
+                false
+        );
+
+        Assertions.assertEquals("EvaluationResult{status=REJECTED, reasons=[Applicant is younger than the minimum age.]}", result.toString());
+    }
 
 }
