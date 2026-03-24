70526d5bcacb2fa9d2058791e63266488592db14
João Pedro da Silva Nascimento <joaopedronascimento171@gmail.com>
2026-03-24 10:25:27 -0300

    New functions of test

--- src/test/java/org/example/ScholarshipEligibilityEvaluatorTest.java
+++ src/test/java/org/example/ScholarshipEligibilityEvaluatorTest.java
@@ -17,8 +17,32 @@ class ScholarshipEligibilityEvaluatorTest {
                 true,
                 false
         );
-
         Assertions.assertEquals("EvaluationResult{status=REJECTED, reasons=[Applicant is younger than the minimum age.]}", result.toString());
     }
 
+    @Test
+    void putAgeUnderThan18(){
+        var age = 16 + random.nextInt(1);
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                age,
+                8.5,
+                92.0,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=MANUAL_REVIEW, reasons=[Applicant is under 18 and requires manual review.]}", result.toString());
+    }
+
+    @Test
+    void putAgeUperThan18(){
+        var age = 18 + random.nextInt(5);
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                age,
+                8.5,
+                92.0,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=MANUAL_REVIEW, reasons=[Applicant is under 18 and requires manual review.]}", result.toString());
+    }
 }
