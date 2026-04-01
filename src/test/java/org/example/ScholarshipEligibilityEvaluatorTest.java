af83e241ea5a00257ab42ffbb5eb2a987af44f33
João Pedro da Silva Nascimento <joaopedronascimento171@gmail.com>
2026-04-01 11:58:49 -0300

    New tests functions

--- src/test/java/org/example/ScholarshipEligibilityEvaluatorTest.java
+++ src/test/java/org/example/ScholarshipEligibilityEvaluatorTest.java
@@ -7,6 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ScholarshipEligibilityEvaluatorTest {
     Random random = new Random();
+
+    @Test
+    void putAttLowerThanMinimum(){
+        var att = random.nextDouble(75.0);
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                18,
+                8.5,
+                att,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=REJECTED, reasons=[Attendance rate is below the minimum required.]}", result.toString());
+    }
+
+    @Test
+    void putGpaLowerThanMinimum(){
+        var gpa = random.nextInt(6);
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                18,
+                gpa,
+                92.0,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=REJECTED, reasons=[GPA is below the minimum required.]}", result.toString());
+    }
+
     @Test
     void putAgeLowerThanMinimum(){
         var age = random.nextInt(15);
@@ -44,5 +71,79 @@ class ScholarshipEligibilityEvaluatorTest {
                 false
         );
         Assertions.assertEquals("EvaluationResult{status=APPROVED, reasons=[Applicant meets all scholarship requirements.]}", result.toString());
+    }
+
+    @Test
+    void putInvalidTypeArgumentGpa(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                    18,
+                    11,
+                    92.0,
+                    true,
+                    false
+            );
+        });
+    }
+
+    @Test
+    void putInvalidTypeArgumentAtt(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                    18,
+                    10,
+                    100.1,
+                    true,
+                    false
+            );
+        });
+    }
+
+    @Test
+    void putTopLimitValueGpa(){
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                18,
+                10.0,
+                92.0,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=APPROVED, reasons=[Applicant meets all scholarship requirements.]}", result.toString());
+    }
+
+    @Test
+    void putLowLimitValueGpa(){
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                18,
+                0.0,
+                92.0,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=REJECTED, reasons=[GPA is below the minimum required.]}", result.toString());
+    }
+
+    @Test
+    void putTopLimitValueAtt(){
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                18,
+                8.5,
+                100.0,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=APPROVED, reasons=[Applicant meets all scholarship requirements.]}", result.toString());
+    }
+
+    @Test
+    void putLowLimitValueAtt(){
+        ScholarshipEligibilityEvaluator.EvaluationResult result = ScholarshipEligibilityEvaluator.evaluateScholarship(
+                18,
+                8.5,
+                0.0,
+                true,
+                false
+        );
+        Assertions.assertEquals("EvaluationResult{status=REJECTED, reasons=[Attendance rate is below the minimum required.]}", result.toString());
     }
 }
